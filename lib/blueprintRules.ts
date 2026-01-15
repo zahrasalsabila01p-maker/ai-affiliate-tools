8c4aaec162f18e137fa73cc80f6c196576ac1cfd
zahrasalsabila01p <zahrasalsabila01p@gmail.com>
2026-01-15 21:57:44 +0700

    Create blueprintRules.ts

---
+++ lib/blueprintRules.ts
@@ -0,0 +1,19 @@
+// Blueprint Rules (Week 1++)
+// Akan diisi saat pindah ke laptop
+
+/*
+Input:
+- Niche
+- Platform
+- Tipe tangan
+- Nama produk
+
+Output:
+- Hook
+- Problem
+- Solusi
+- CTA
+- Script visual
+- Micro insight
+- Coach note
+*/
